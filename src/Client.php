e07724083edd3f6979e9546c41ab5e017ad9f69b
Simo Heinonen <simo@dudgeon.fi>
2017-12-26 01:21:20 +0200

    Baa

--- src/Client.php
+++ src/Client.php
@@ -25,53 +25,7 @@ class Client
             $options = ['query' => ['game-names' => implode(',', $gameNames)]];
         }
 
-
-        $wtfgsr = <<< JSON
-{
-      "rows": [
-        {
-          "id": "0",
-          "tvChannel": "Viasat",
-          "outcome": {
-            "home": {
-              "id": "Man Utd",
-              "name": "Manchester U"
-            },
-            "away": {
-              "id": "Burnley",
-              "name": "Burnley"
-            }
-          },
-          "eventId": "91107453",
-          "additionalPrizeTier": true
-        },
-        {
-          "id": "14",
-          "outcome": {
-            "home": {
-              "id": "Chelsea",
-              "name": "Chelsea"
-            },
-            "away": {
-              "id": "Brighton",
-              "name": "Brighton"
-            }
-          },
-          "eventId": "91107565",
-          "additionalPrizeTier": true
-        }
-    ]
-}
-JSON;
-
-
-        $wtf = $this->serializer->deserialize($wtfgsr, Rows::class, 'json');
-
-#        dump($wtf); exit;
-
         $res = $this->httpClient->get('api/v1/sport-games/draws', $options);
-
-#        echo $res->getBody(); exit;
 
         return $this->serializer->deserialize($res->getBody(), Draws::class, 'json');
     }
